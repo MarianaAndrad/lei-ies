24d5dde3576d02d667cfe297f5910d2de719fe31
mariana <marianaandrade@ua.pt>
2022-09-28 17:30:37 +0100

    Resolução das alienas d até à k do exericicio 1.2

--- Lab01/Lab1.2/MyWeatherRadar/src/main/java/pt/ua/deti/WeatherStarter.java
+++ Lab01/Lab1.2/MyWeatherRadar/src/main/java/pt/ua/deti/WeatherStarter.java
@@ -14,6 +14,14 @@ public class WeatherStarter {
     private static final int CITY_ID_AVEIRO = 1010500;
 
     public static void  main(String[] args ) {
+        int City_ID = -1;
+        if (args.length != 1){
+            System.out.println("No city id provided, using Aveiro as default");
+            City_ID = CITY_ID_AVEIRO;
+        } else{
+            System.out.println("ID da Cidade: " + args[0]);
+            City_ID = Integer.parseInt(args[0]);
+        }
 
         // get a retrofit instance, loaded with the GSon lib to convert JSON into objects
         Retrofit retrofit = new Retrofit.Builder()
@@ -24,18 +32,31 @@ public class WeatherStarter {
         // create a typed interface to use the remote API (a client)
         IpmaService service = retrofit.create(IpmaService.class);
         // prepare the call to remote endpoint
-        Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID_AVEIRO);
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(City_ID);
 
         try {
             Response<IpmaCityForecast> apiResponse = callSync.execute();
             IpmaCityForecast forecast = apiResponse.body();
 
             if (forecast != null) {
-                CityForecast firstDay = forecast.getData().listIterator().next();
+                //colocar para diferentes datas
+                System.out.println("Na cidade com ID " + forecast.getGlobalIdLocal() + " no país " + forecast.getCountry()+ ".");
 
-                System.out.printf( "max temp for %s is %4.1f %n",
-                        firstDay.getForecastDate(),
-                        Double.parseDouble(firstDay.getTMax()));
+                System.out.println("----------------------------------------------------------------------------------------------------------------------------");
+                System.out.printf("%10s | %10s | %10s | %10s | %10s | %10s | %10s %n", "DATE", "TEMP MAX", "TEMP MIN", "PROB PRECI", "DIR VENTO", "VEL VENTO", "ID TEMPO");
+                System.out.println("----------------------------------------------------------------------------------------------------------------------------");
+                for (CityForecast day : forecast.getData()) {
+                    System.out.printf("%10s | %8.1fºC | %8.1fºC | %8.2f%% | %10s | %10s | %10s %n",
+                            day.getForecastDate(),
+                            Double.parseDouble(day.getTMax()),
+                            Double.parseDouble(day.getTMin()),
+                            Double.parseDouble(day.getPrecipitaProb()),
+                            day.getPredWindDir(),
+                            day.getClassWindSpeed(),
+                            day.getIdWeatherType());
+                }
+                System.out.println("----------------------------------------------------------------------------------------------------------------------------");
+
             } else {
                 System.out.println( "No results for this request!");
             }
