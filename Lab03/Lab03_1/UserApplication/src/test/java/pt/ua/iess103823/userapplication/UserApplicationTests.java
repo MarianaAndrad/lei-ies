c5f35848cc5dbb4286c89ae007be960bd2e99b29
mariana <marianaandrade@ua.pt>
2022-10-18 15:16:59 +0100

    new: criar projeto

---
+++ Lab03/Lab03_1/UserApplication/src/test/java/pt/ua/iess103823/userapplication/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.iess103823.userapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
